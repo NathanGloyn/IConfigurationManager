75054b7ef3f36dd33b4cb964fc961783c671934a
NathanGloyn <nathans.dropbox@gmail.com>
2011-04-06 21:49:46 +0100

    Renamed projects and solution to better reflect purpose of each

---
+++ IConfigurationManager/Configuration.Core.Test/Helper/SampleSectionProvider.cs
@@ -0,0 +1,36 @@
+﻿using System.Configuration;
+
+namespace ConfigurationManagerWrapper.Test
+{
+    public class SampleSectionProvider : ConfigurationSection
+    {
+        [ConfigurationProperty("global")]
+        public bool Global
+        {
+            get { return (bool)this["global"]; }
+        }
+
+        // Create a "font" element.
+        [ConfigurationProperty("sampleElement")]
+        public SampleElement SampleElement
+        {
+            get { return (SampleElement)this["sampleElement"]; }
+            //set { this["sampleElement"] = value; }
+        }
+    }
+
+    public class SampleElement : ConfigurationElement
+    {
+        [ConfigurationProperty("name")]
+        public string Name
+        {
+            get { return this["name"] as string; }
+        }
+
+        [ConfigurationProperty("value")]
+        public string Value
+        {
+            get { return this["value"] as string; }
+        }
+    }
+}
---
+++ IConfigurationManager/Configuration.Core.Test/When_using_WebConfigurationManagerAdapter_to_get_custom_section.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using Configuration.Core;
+
+namespace ConfigurationManagerWrapper.Test
+{
+    [TestFixture]
+    public class When_using_WebConfigurationManagerAdapter_to_get_custom_section
+    {
+        private WebConfigurationManagerAdapter _target;
+        private SampleSectionProvider _provider;
+
+        [SetUp]
+        public void TestSetUp()
+        {
+            _target = new WebConfigurationManagerAdapter();
+        }
+
+        private void LoadProvider()
+        {
+            _provider = _target.GetSection<SampleSectionProvider>("sampleSection");            
+        }
+
+        [Test]
+        public void Should_return_instance_of_correct_type()
+        {
+            ConfigurationFileLoader.LoadConfigurationFile(TestConfig.CustomSectionProviderDefinitionOnly);
+            LoadProvider();
+            Assert.IsNotNull(_provider);
+        }
+
+        [Test]
+        public void Should_return_value_of_section_attribute_global()
+        {
+            ConfigurationFileLoader.LoadConfigurationFile(TestConfig.CustomSectionProviderSectionAttribute);
+            LoadProvider();    
+            Assert.IsTrue(_provider.Global);
+        }
+
+        [Test]
+        public void Should_be_able_to_get_name_of_sample_element()
+        {
+            ConfigurationFileLoader.LoadConfigurationFile(TestConfig.CustomSectionProviderWithValues);
+            LoadProvider();
+            Assert.AreEqual("abc", _provider.SampleElement.Name);
+        }
+
+        [Test]
+        public void Should_be_able_to_get_value_of_sample_element()
+        {
+            ConfigurationFileLoader.LoadConfigurationFile(TestConfig.CustomSectionProviderWithValues);
+            LoadProvider();
+            Assert.AreEqual("123", _provider.SampleElement.Value);
+        }
+    }
+}
